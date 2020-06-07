5dd9dda9a9072d1a17359f186caf92b441c43785
Bruno.Silva <brs.rsilva@gmail.com>
2020-06-07 15:51:05 -0300

    box control options login

--- src/js/header.js
+++ src/js/header.js
@@ -2,12 +2,22 @@ $(document).ready(() =>{
 
     //script para fazer apresentação do sub-menu
     //não está sendo usado. (somente para estudo)
-    $(".l2").mouseenter(function() {
-        $(this).addClass('sub-effects');
-        $(this).find('.sub1').fadeIn(600);
-    });
-    $(".l2").mouseleave(function() {
-        $(this).removeClass('sub-effects');
-        $(this).find('.sub1').hide();
+    // $(".l2").mouseenter(function() {
+    //     $(this).addClass('sub-effects');
+    //     $(this).find('.sub1').fadeIn(600);
+    // });
+    // $(".l2").mouseleave(function() {
+    //     $(this).removeClass('sub-effects');
+    //     $(this).find('.sub1').hide();
+    // });
+
+
+    $(".btn-login").click(function() {
+        var box = $(".control-login-options");
+        if(box.hasClass("d-none")){
+            box.removeClass("d-none");
+        }else{
+            box.addClass("d-none");
+        }
     });
 });
